7584e7444dc35deab0cc80b8fe366ed0bd4c9209
Pablo Bee Olmedo <pliverpool96@gmail.com>
2018-12-12 12:04:09 +1100

    New folder

--- fits_file_reader.py
+++ fits_file_reader.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob
 from matplotlib import pyplot as plt
 
-numbertorun = 50
+numbertorun = 100
 
 # t = Table.read(filename,hdu=1)
 # Initialise
@@ -16,21 +16,24 @@ max_loglam = []
 
 for i in range(numbertorun):
     print(i)
-    fname = glob.glob("/Users/Pablo/OneDrive - UNSW/4th Year/Summer Physics /Research/02_Data_Files/Spectra/*.fits")[i]
+    fname = glob.glob("../02_Data_Files/Spectra/*.fits")[i]
     table = Table.read(fname,hdu=1)
     loglam = table["loglam"].data
     flux = table["flux"].data
-
     flux_values.append(flux)
     loglam_values.append(loglam)
+
+    plt.plot(loglam,flux)
     min_loglam.append(np.min(loglam))
     max_loglam.append(np.max(loglam))
 
-
+'''
 plt.scatter(list(range(numbertorun)),min_loglam)
 plt.scatter(list(range(numbertorun)),max_loglam)
-plt.plot([0,50],[np.mean(min_loglam),np.mean(min_loglam)])
-plt.plot([0,50],[np.mean(max_loglam),np.mean(max_loglam)])
+plt.plot([0,numbertorun],[np.mean(min_loglam),np.mean(min_loglam)])
+plt.plot([0,numbertorun],[np.mean(max_loglam),np.mean(max_loglam)])
 plt.show()
+'''
 
+print(np.mean(min_loglam))
 print(np.mean(max_loglam))
