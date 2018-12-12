a305065e022fde3c2eafcb1e3f92350513594fc0
Pablo Bee Olmedo <pliverpool96@gmail.com>
2018-12-12 11:18:29 +1100

    New files

--- fits_file_reader.py
+++ fits_file_reader.py
@@ -16,7 +16,7 @@ max_loglam = []
 
 for i in range(numbertorun):
     print(i)
-    fname = glob.glob("/02_Data_Files/Spectra/*.fits")[i]
+    fname = glob.glob("/Users/Pablo/OneDrive - UNSW/4th Year/Summer Physics /Research/02_Data_Files/Spectra/*.fits")[i]
     table = Table.read(fname,hdu=1)
     loglam = table["loglam"].data
     flux = table["flux"].data
@@ -26,11 +26,11 @@ for i in range(numbertorun):
     min_loglam.append(np.min(loglam))
     max_loglam.append(np.max(loglam))
 
-'''
+
 plt.scatter(list(range(numbertorun)),min_loglam)
 plt.scatter(list(range(numbertorun)),max_loglam)
 plt.plot([0,50],[np.mean(min_loglam),np.mean(min_loglam)])
 plt.plot([0,50],[np.mean(max_loglam),np.mean(max_loglam)])
 plt.show()
-'''
+
 print(np.mean(max_loglam))
