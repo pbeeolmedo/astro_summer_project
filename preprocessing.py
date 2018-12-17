fe94d545d90320e8183cf6df48920039dbb3847f
Pablo Bee Olmedo <pliverpool96@gmail.com>
2018-12-17 14:49:14 +1100

    big big changes

---
+++ preprocessing.py
@@ -0,0 +1,45 @@
+from astropy.table import Table
+import numpy as np
+import glob
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+import pandas as pd
+from scipy import constants as const
+from stellarpy import Star
+
+flux_values = []
+good_spec_info=[]
+subclasses=[]
+chi_vals=[]
+num_points=[]
+MAX_NUM_FILES = len(glob.glob("Data_Files/Spectra/*.fits"))
+numbertorun = int(input(f"Enter number of files to run (max = {MAX_NUM_FILES}) : "))
+
+
+UPPER_CUTOFF_LOGLAM = 3.95
+LOWER_CUTOFF_LOGLAM = 3.59
+SPECTRUM_LENGTH = 3599
+MAX_CHI = 3
+
+LOGLAM_GRID = np.linspace(LOWER_CUTOFF_LOGLAM, UPPER_CUTOFF_LOGLAM, SPECTRUM_LENGTH)
+print(f" The spectrum grid values (of wavelengths) to be used for the pre-processing are : {LOGLAM_GRID}")
+
+fits_folder = "Data_Files/Spectra"
+
+for i in range(numbertorun):
+	star = Star(glob.glob(fits_folder + "/*.fits")[i])
+	if(star.subclass and star.chi_sq <= MAX_CHI and star.plate_quality == "good"):
+		if np.min(star.loglam_restframe) <= LOWER_CUTOFF_LOGLAM and np.max(star.loglam_restframe) >= UPPER_CUTOFF_LOGLAM:
+			flux_interp = np.interp(LOGLAM_GRID, star.loglam_restframe, star.flux)
+			normalised_flux = flux_interp/np.max(flux_interp)
+			flux_values.append(normalised_flux)
+			subclasses.append(star.subclass)
+			print("Subclass: "+ star.subclass)
+			#star.spectrum_plot
+		else:
+			print("Bad wavelength Range")
+	else:
+		print(f"Iteration skipped : subclass = {star.subclass} chi_sq = {star.chi_sq} plate quality = {star.plate_quality}")
+
+processed_data_df = pd.DataFrame(flux_values)
---
+++ stellarpy.py
@@ -0,0 +1,77 @@
+from astropy.table import Table
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+
+class Spectrum_2D(object):
+    # Spectrum
+    def __init__(self,loglam_obs,flux_obs):
+        self.loglam_obs = loglam_obs
+        self.flux_obs = flux_obs
+
+class Star(object):
+    # Star ...
+    def __init__(self,filename):
+        if filename is None:
+            raise FileNotFoundError("FITS file not specified")
+        self.filename = filename
+        self.hdu1 = Table.read(self.filename,hdu=1)
+        self.hdu2 = Table.read(self.filename,hdu=2)
+
+    # HDU 2 obtained properties
+    @property
+    def plate_quality(self):
+        plate_q = self.hdu2['PLATEQUALITY'].data[0]
+        return plate_q.strip().decode("utf-8")
+
+    @property
+    def subclass(self):
+        subclass = self.hdu2['SUBCLASS'].data[0]
+        return subclass.strip().decode("utf-8")
+
+    @property
+    def ra(self):
+        return self.hdu2['PLUG_RA'].data
+
+    @property
+    def dec(self):
+        return self.hdu2['PLUG_DEC'].data
+
+    @property
+    def chi_sq(self):
+        return self.hdu2['RCHI2'].data[0]
+
+    @property
+    def z(self):
+        return self.hdu2['Z'].data[0]
+
+    # HDU 1 obtained properties
+    @property
+    def loglam(self):
+        return self.hdu1['loglam'].data
+
+    @property
+    def flux(self):
+        return self.hdu1['flux'].data
+
+    # Other
+    @property
+    def loglam_restframe(self):
+        loglam_rf = self.loglam - np.log10(self.z+1)
+        return loglam_rf
+
+    def spectrum_plot(self):
+        ''' Plots flux vs restframe wavelengths'''
+        plt.subplot(1,2,1)
+        plt.scatter(self.loglam, self.flux, c='green', s=1)
+        plt.xlabel("Loglam (observed) ")
+        plt.ylabel("Flux")
+        plt.title("Original Spectra for "+ self.subclass)
+        plt.subplot(1,2,2)
+        plt.scatter(self.loglam_restframe,self.flux, c='red', s=1)
+        plt.xlabel("Loglam (restframe)")
+        plt.ylabel("Flux")
+        plt.title("Shifted Spectra for "+ self.subclass)
+        plt.tight_layout()
+        return plt.show()
