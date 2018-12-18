4becf2928f2ab81729d44081a079fa59263ac280
Lauren Taylor <lauren.y.taylor96@gmail.com>
2018-12-18 13:59:14 +1100

    added fits handler file

---
+++ Small_Data_Files/fits_handler_test.py
@@ -0,0 +1,17 @@
+from astropy.table import Table
+from astropy.io import fits
+import numpy as np
+
+t = Table.read('spec-3106-54714-0471.fits',2)
+hdu = fits.open('spec-3106-54714-0471.fits')
+print(hdu[2].header)
+print(t['Z'])
+
+
+#print(hdu[2].header)
+
+#loglam = [1,2,3,4,5,6,7,8,9]
+#b = np.array(a)
+
+#c = np.where((loglam<max_cutoff) & (loglam>min_cutoff))
+#print(b[c])
